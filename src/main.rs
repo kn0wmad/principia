5cb514f265c2e6c799edf7dd1fe91b6fbf517961
kn0wmad <kn0wmad@protonmail.com>
2020-10-29 21:59:50 -0600

    Blockchain and block verification tests

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,49 @@
 use blockchainlib::*;
 
 fn main() {
-    let mut block = Block::new(0, 0, vec![0; 32],
-        0, "Genesis block".to_owned(), 0x00000fffffffffffffffffffffffffff);
+    let difficulty = 0x0000ffffffffffffffffffffffffffff;
+
+    let mut block = Block::new(0, now(), vec![0; 32],
+        0, "Genesis block".to_owned(), difficulty);
     
     println!("Principia");
 
-    block.hash = block.hash();
-
-    println!("{:?}", &block);
-
     block.mine();
-
     println!("{:?}", &block);
+
+    let mut last_hash = block.hash.clone();
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    println!("Verify: {}", &blockchain.verify());
+
+    for i in 1..=10 {
+        let mut block = Block::new(i, now(), last_hash,
+        0, "Another block".to_owned(), difficulty);
+    
+        block.mine();
+        println!("Mined block {:?}", &block);
+
+        last_hash = block.hash.clone();
+
+        blockchain.blocks.push(block);
+        println!("Verify: {}", &blockchain.verify());
+    }
+        // tests for invalid blocks
+        
+        /*
+        blockchain.blocks[3].hash[8] += 1; 
+        println!("Verify: {}", &blockchain.verify());
+
+        blockchain.blocks[3].index = 4;
+        println!("Verify: {}", &blockchain.verify());
+        
+        blockchain.blocks[3].payload = "Nope".to_owned();
+        println!("Verify: {}", &blockchain.verify());
+        
+        blockchain.blocks[3].prev_block_hash[18] = 8;
+        println!("Verify: {}", &blockchain.verify());
+        */
 }
